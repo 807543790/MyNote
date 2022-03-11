e1d949dc7928628f4bfbc01b50100196861d1180
zhangbin <807553790@qq.com>
2022-03-11 15:22:56 +0800

    算法

--- java-note/src/main/java/com/zhangbin/算法/素数.java
+++ java-note/src/main/java/com/zhangbin/算法/素数.java
@@ -2,7 +2,7 @@ package com.zhangbin.算法;
 
 public class 素数 {
 //    题目：判断101-200之间有多少个素数，并输出所有素数。
-//    程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，如果能被整除， 则表明此数不是素数，反之是素数。
+//    程序分析：判断素数的方法：用一个数分别去除2到sqrt(这个数)，如果能被整除， 则表明此数不是素数，反之是素数。！
     public static void main(String[] args) {
         String s = "";
         s.equals("2");
